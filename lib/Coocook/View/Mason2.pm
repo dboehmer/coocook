d43c44781f0bfa827c5582f185e5c713f0885b3b
Daniel Böhmer <post@daniel-boehmer.de>
2015-05-04 15:52:21 +0200

    Revert "Create Mason2 view"
    This reverts commit 600e16882b4c8d728777af05d8c66773a5290713.

--- lib/Coocook/View/Mason2.pm
+++
@@ -1,7 +0,0 @@
-package Coocook::View::Mason2;
-
-use strict;
-use warnings;
-use base qw(Catalyst::View::Mason2);
-
-__PACKAGE__->config();
--- t/view_Mason2.t
+++
@@ -1,8 +0,0 @@
-use strict;
-use warnings;
-use Test::More;
-
-
-BEGIN { use_ok 'Coocook::View::Mason2' }
-
-done_testing();
