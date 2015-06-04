c57577d53fc8dfef7038dab65d6fc66bbcd01152
Daniel Böhmer <post@daniel-boehmer.de>
2015-06-04 18:22:01 +0200

    Enable UTF8 for Model::Schema

--- lib/Coocook/Model/Schema.pm
+++ lib/Coocook/Model/Schema.pm
@@ -6,6 +6,11 @@ extends 'Catalyst::Model::DBIC::Schema';
 
 __PACKAGE__->meta->make_immutable;
 
-__PACKAGE__->config( schema_class => 'Coocook::Schema', );
+__PACKAGE__->config(
+    connect_info => {
+        sqlite_unicode => 1,
+    },
+    schema_class => 'Coocook::Schema',
+);
 
 1;
