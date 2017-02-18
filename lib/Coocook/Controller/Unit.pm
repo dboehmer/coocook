f3c1fcd34ee67b3f4d4ed51ae9b31856a88a0a19
Christina Wagner <none>
2017-02-18 14:24:52 +0100

    Check input when editing units

--- lib/Coocook/Controller/Unit.pm
+++ lib/Coocook/Controller/Unit.pm
@@ -42,28 +42,21 @@ sub create : Local : POST {
 	my $short_name = scalar $c->req->param('short_name');
 	my $long_name = scalar $c->req->param('long_name');
 	my $to_quantity_default = scalar $c->req->param('to_quantity_default');
-	if (length($short_name) <= 0){
-		$c->response->redirect(
-                $c->uri_for( '/units', { error => "Cannot create unit with empty short name!" } ) );
-	} elsif (length($long_name) <= 0){
-		$c->response->redirect(
-               $c->uri_for( '/units', { error => "Cannot create unit with empty long name!" } ) );
-	} elsif (!(looks_like_number( $to_quantity_default ))) {
-		$c->response->redirect(
-               $c->uri_for( '/units', { error => "Please supply a valid number in 'Factor to quantity's default unit'.\n E.g.: 1.33" } ) );
-	} else {
-		$c->model('Schema::Unit')->create(
+	my $input_okay = $self->check_input($c, {short_name => $short_name, long_name => $long_name, to_quantity_default => $to_quantity_default, current_page => '/units'});
+	if($input_okay){
+        $c->model('Schema::Unit')->create(
         {
-            short_name          => scalar $c->req->param('short_name'),
-            long_name           => scalar $c->req->param('long_name'),
+            short_name          => $short_name,
+            long_name           => $long_name,
             quantity            => scalar $c->req->param('quantity') || undef,
-            to_quantity_default => scalar $c->req->param('to_quantity_default')
+            to_quantity_default => $to_quantity_default
               || undef,
             space => scalar $c->req->param('space') ? '1' : '0',
         }
 		);
 		$c->detach('redirect');
-	}
+    }
+
 }
 
 sub delete : Local : Args(1) : POST {
@@ -80,16 +73,23 @@ sub make_quantity_default : Local Args(1) : POST {
 
 sub update : Local : Args(1) : POST {
     my ( $self, $c, $id ) = @_;
-    $c->model('Schema::Unit')->find($id)->update(
+    my $unit = $c->model('Schema::Unit')->find($id);
+    my $short_name = scalar $c->req->param('short_name');
+	my $long_name = scalar $c->req->param('long_name');
+	my $to_quantity_default = scalar $c->req->param('to_quantity_default');
+    my $input_okay = $self->check_input($c, {short_name => $short_name, long_name => $long_name, to_quantity_default => $to_quantity_default, current_page => "/unit/$id"});
+    if($input_okay){
+        $unit->update(
         {
-            short_name => scalar $c->req->param('short_name'),
-            long_name  => scalar $c->req->param('long_name'),
-            to_quantity_default => scalar $c->req->param('to_quantity_default')
+            short_name => $short_name,
+            long_name  => $long_name ,
+            to_quantity_default => $to_quantity_default
               || undef,
             space => scalar $c->req->param('space') ? '1' : '0',
         }
     );
     $c->detach('redirect');
+    }
 }
 
 sub edit : Path: Args(1) : GET {
@@ -104,6 +104,29 @@ sub redirect : Private {
     my ( $self, $c ) = @_;
 
     $c->response->redirect( $c->uri_for_action( $self->action_for('index') ) );
+}
+
+#check input of unit when creating and updating a unit
+sub check_input : Private {
+	my ( $self, $c, $args ) = @_;
+	my $shortname = $args->{short_name};
+	my $longname = $args->{long_name};
+    my $to_quantity_default = $args->{to_quantity_default};
+    my $current_page = $args->{current_page};
+    my $result = 0;
+    if (length($shortname) <= 0){
+		$c->response->redirect(
+                $c->uri_for( $current_page, { error => "Cannot create unit with empty short name!" } ) );
+	} elsif (length($longname) <= 0){
+		$c->response->redirect(
+               $c->uri_for( $current_page, { error => "Cannot create unit with empty long name!" } ) );
+	} elsif (!(looks_like_number( $to_quantity_default ))) {
+		$c->response->redirect(
+               $c->uri_for( $current_page, { error => "Please supply a valid number in 'Factor to quantity's default unit'.\n E.g.: 1.33" } ) );
+	} else {
+        $result = 1;
+    }
+    return $result;
 }
 
 =encoding utf8
