dboehmer/coocook

commit b305724c4f7a28aab029fb68f5ea05803e04d2f6

Enable Helper::ResultSet::IgnoreWantarray, explicitly fetch rows
Context sensivity for resultset methods is problematic. This
makes all resultset methods return ResultSet objects and fetch
the elements explicitly, e.g. with ->all().

lib/Coocook/Controller/Recipe.pm
@@ -27,7 +27,7 @@ Catalyst Controller.
 sub index : Path('/recipes') : Args(0) {
     my ( $self, $c ) = @_;
 
-    $c->stash( recipes => $c->model('Schema::Recipe')->sorted_rs );
+    $c->stash( recipes => $c->model('Schema::Recipe')->sorted );
 }
 
 sub edit : Path : Args(1) {
@@ -47,8 +47,8 @@ sub edit : Path : Args(1) {
     $c->stash(
         recipe      => $recipe,
         ingredients => [ $ingredients->all ],
-        articles    => [ $c->model('Schema::Article')->sorted ],
-        units       => [ $c->model('Schema::Unit')->sorted ],
+        articles    => [ $c->model('Schema::Article')->sorted->all ],
+        units       => [ $c->model('Schema::Unit')->sorted->all ],
     );
 }
 

lib/Coocook/Controller/Dish.pm
@@ -35,9 +35,9 @@ sub edit : Path Args(1) {
 
     $c->stash(
         dish          => $dish,
-        ingredients   => [ $dish->ingredients_ordered ],
+        ingredients   => [ $dish->ingredients_ordered->all ],
         articles      => [ $c->model('Schema::Article')->all ],
-        units         => [ $c->model('Schema::Unit')->all ],
+        units         => [ $c->model('Schema::Unit')->sorted->all ],
         prepare_meals => [ $prepare_meals->all ],
     );
 }
@@ -138,7 +138,7 @@ sub update : Local Args(1) POST {
             my $tags = $c->model('Schema::Tag')->from_names( scalar $c->req->param('tags') );
             $dish->set_tags( [ $tags->all ] );
 
-            for my $ingredient ( $dish->ingredients ) {
+            for my $ingredient ( $dish->ingredients->all ) {
                 if ( scalar $c->req->param( 'delete' . $ingredient->id ) ) {
                     $ingredient->delete;
                     next;

lib/Coocook/Controller/Project.pm
@@ -62,7 +62,7 @@ sub edit : Path : Args(1) {
     $c->stash(
         default_date => $default_date,
         project      => $project,
-        recipes      => [ $c->model('Schema::Recipe')->search( undef, { order_by => 'name' } ) ],
+        recipes      => [ $c->model('Schema::Recipe')->search( undef, { order_by => 'name' } )->all ],
         days         => $days,
     );
 }

lib/Coocook/Controller/PurchaseList.pm
@@ -45,7 +45,7 @@ sub index : Path('/purchase_lists') Args(0) {
 
     $c->stash(
         default_date => $default_date,
-        lists        => $lists->search_rs( undef, { order_by => 'date' } ),
+        lists        => $lists->search( undef, { order_by => 'date' } ),
     );
 }
 
@@ -90,8 +90,8 @@ sub edit : Path Args(1) {
             article          => $articles{$article},
             unit             => $units{$unit},
             comment          => $item->comment,
-            ingredients      => [ $item->ingredients ],
-            convertible_into => [ $item->convertible_into ],
+            ingredients      => [ $item->ingredients->all ],
+            convertible_into => [ $item->convertible_into->all ],
           };
     }
 

lib/Coocook/Controller/Quantity.pm
@@ -24,7 +24,7 @@ Catalyst Controller.
 sub index : Path('/quantities') : Args(0) {
     my ( $self, $c ) = @_;
 
-    $c->stash( quantities => $c->model('Schema::Quantity')->sorted_rs );
+    $c->stash( quantities => $c->model('Schema::Quantity')->sorted );
 }
 
 sub create : Local : POST {

lib/Coocook/Controller/ShopSection.pm
@@ -26,7 +26,7 @@ Catalyst Controller.
 sub index : Path('/shop_sections') : Args(0) {
     my ( $self, $c ) = @_;
 
-    $c->stash( shop_sections => scalar $c->model('Schema::ShopSection')->with_article_count->sorted );
+    $c->stash( shop_sections => $c->model('Schema::ShopSection')->with_article_count->sorted );
 }
 
 sub create : Local Args(0) POST {

lib/Coocook/Controller/Tag.pm
@@ -25,8 +25,8 @@ sub index : Path('/tags') : Args(0) {
     my ( $self, $c ) = @_;
 
     $c->stash(
-        groups     => $c->model('Schema::TagGroup')->sorted_rs,
-        other_tags => $c->model('Schema::Tag')->ungrouped->sorted_rs,
+        groups     => $c->model('Schema::TagGroup')->sorted,
+        other_tags => $c->model('Schema::Tag')->ungrouped->sorted,
     );
 }
 
@@ -34,7 +34,7 @@ sub edit : Path Args(1) {
     my ( $self, $c, $id ) = @_;
     $c->stash(
         tag    => $c->model('Schema::Tag')->find($id),
-        groups => $c->model('Schema::TagGroup')->sorted_rs,
+        groups => $c->model('Schema::TagGroup')->sorted,
     );
 }
 

lib/Coocook/Controller/Unit.pm
@@ -26,14 +26,14 @@ sub index : Path('/units') : Args(0) {
     my ( $self, $c ) = @_;
 
     $c->stash(
-        units => $c->model('Schema::Unit')->search_rs(
+        units => $c->model('Schema::Unit')->search(
             undef,
             {
                 join     => 'quantity',
                 order_by => [qw< quantity.name short_name >]
             }
         ),
-        quantities => [ $c->model('Schema::Quantity')->sorted ],
+        quantities => [ $c->model('Schema::Quantity')->sorted->all ],
     );
 }
 

commit b137d1410640200b0dacb3a5396020c1825bfbe9

Fix missing ->all call
Also broken by

    b305724 Enable Helper::ResultSet::IgnoreWantarray, explicitly fetch rows

lib/Coocook/Controller/Recipe.pm
@@ -117,7 +117,7 @@ sub update : Local : Args(1) : POST {
                 );
 
                 # ingredients
-                for my $ingredient ( $recipe->ingredients ) {
+                for my $ingredient ( $recipe->ingredients->all ) {
                     if ( scalar $c->req->param( 'delete' . $ingredient->id ) ) {
                         $ingredient->delete;
                         next;