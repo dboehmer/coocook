1bfb93f485c09a2e79bdee48cf4ddd7348f03673
Daniel Böhmer <post@daniel-boehmer.de>
2017-08-15 00:52:11 +0200

    Add ugly hack for using in-memory SQLite for tests

--- lib/Coocook/Model/Schema.pm
+++ lib/Coocook/Model/Schema.pm
@@ -5,6 +5,33 @@ use MooseX::MarkAsMethods autoclean => 1;
 
 extends 'Catalyst::Model::DBIC::Schema';
 
+### use in-memory SQLite during tests ###
+
+around BUILDARGS => sub {
+    my $orig  = shift;
+    my $class = shift;
+
+    if ( $ENV{HARNESS_ACTIVE} ) {
+        if ( @_ == 1 ) {
+            $_[0]->{connect_info} = "dbi:SQLite::memory:";
+        }
+        else {
+            push @_, ( connect_info => "dbi:SQLite::memory:" );
+        }
+    }
+
+    return $class->$orig(@_);
+};
+
+sub BUILD {
+    my $self = shift;
+
+    $ENV{HARNESS_ACTIVE}
+      and $self->schema->deploy;
+}
+
+######
+
 __PACKAGE__->meta->make_immutable;
 
 __PACKAGE__->config(
