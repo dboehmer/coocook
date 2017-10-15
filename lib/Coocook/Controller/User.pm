36402e7571dda9ba7b458535b6b159b9df42d809
Daniel Böhmer <post@daniel-boehmer.de>
2017-10-15 02:25:00 +0200

    Add form to change logged in user's display name

--- lib/Coocook/Controller/User.pm
+++ lib/Coocook/Controller/User.pm
@@ -17,17 +17,34 @@ Catalyst Controller.
 
 =cut
 
-sub show : GET Path Args(1) {
+sub base : Chained('/') PathPart('user') CaptureArgs(1) {
     my ( $self, $c, $name ) = @_;
 
-    my $user = $c->model('DB::User')->find( { name => $name } );
+    $c->stash( user => $c->model('DB::User')->find( { name => $name } ) );
+}
 
-    $c->stash(
-        user     => $user,
-        projects => [ $user->projects->all ],
-    );
+sub show : GET Chained('base') Args(0) {
+    my ( $self, $c ) = @_;
+
+    my $user = $c->stash->{user};
+
+    $c->stash( projects => [ $user->projects->all ] );
 
     $c->escape_title( User => $user->display_name );
+}
+
+sub update : POST Chained('base') Args(0) {
+    my ( $self, $c ) = @_;
+
+    my $user = $c->stash->{user};
+
+    $user->update(
+        {
+            display_name => scalar $c->req->param('display_name'),
+        }
+    );
+
+    $c->response->redirect( $c->uri_for( $self->action_for('show'), [ $user->name ] ) );
 }
 
 __PACKAGE__->meta->make_immutable;
