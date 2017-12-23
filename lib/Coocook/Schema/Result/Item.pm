dboehmer/coocook

commit 75f131d83d022730929ea4ba96c4ba83e4764568

Replace cross table 'ingredients_items' with FK column 'item'
A cross table with no additional columns and 1:1 relationships
in both directions is pointless. Don't why I added it in the first
place ...

Also Result::Item->convert unnecessarily create a new 'item'
result. Instead the existing one is now updated while converting
it.

lib/Coocook/Schema/Result/Item.pm
@@ -35,8 +35,7 @@ __PACKAGE__->belongs_to(
     }
 );
 
-__PACKAGE__->has_many( ingredients_items => 'Coocook::Schema::Result::IngredientItem', 'item' );
-__PACKAGE__->many_to_many( ingredients => ingredients_items => 'ingredient' );
+__PACKAGE__->has_many( ingredients => 'Coocook::Schema::Result::DishIngredient', 'item' );
 
 __PACKAGE__->meta->make_immutable;
 
@@ -50,25 +49,11 @@ sub convert {
 
     my $factor = $unit1->to_quantity_default / $unit2->to_quantity_default;
 
-    $self->result_source->schema->txn_do(
-        sub {
-            my $item2 = $self->result_source->resultset->add_or_create(
-                {
-                    purchase_list => $self->get_column('purchase_list'),
-                    article       => $self->get_column('article'),
-                    unit          => $unit2->id,
-
-                    # checking if unit is quantity default involved fetching the quantity ...
-                    value  => $self->value * $factor,
-                    offset => $self->offset * $factor,
-                }
-            ) or return;
-
-            $item2->add_to_ingredients($_) for $self->ingredients->all;
-
-            $self->delete;
-
-            return $item2;
+    $self->update(
+        {
+            unit   => $unit2->id,
+            value  => $self->value * $factor,
+            offset => $self->offset * $factor,
         }
     );
 }

lib/Coocook/Schema.pm
@@ -1,6 +1,6 @@
 package Coocook::Schema;
 
-our $VERSION = 6;    # version of schema definition, not software version!
+our $VERSION = 7;    # version of schema definition, not software version!
 
 use Moose;
 use MooseX::MarkAsMethods autoclean => 1;

lib/Coocook/Schema/Result/DishIngredient.pm
@@ -18,6 +18,7 @@ __PACKAGE__->add_columns(
     unit     => { data_type => 'int' },
     value    => { data_type => 'real' },
     comment  => { data_type => 'text' },
+    item     => { data_type => 'int', is_nullable       => 1 },    # from purchase list
 );
 
 __PACKAGE__->set_primary_key("id");
@@ -28,6 +29,7 @@ __PACKAGE__->grouping_column('dish');
 
 __PACKAGE__->belongs_to( article => 'Coocook::Schema::Result::Article' );
 __PACKAGE__->belongs_to( dish    => 'Coocook::Schema::Result::Dish' );
+__PACKAGE__->belongs_to( item    => 'Coocook::Schema::Result::Item' );
 __PACKAGE__->belongs_to( unit    => 'Coocook::Schema::Result::Unit' );
 __PACKAGE__->belongs_to(
     article_unit => 'Coocook::Schema::Result::ArticleUnit',
@@ -36,10 +38,6 @@ __PACKAGE__->belongs_to(
         'foreign.unit'    => 'self.unit',
     }
 );
-
-__PACKAGE__->has_many(
-    ingredients_items => 'Coocook::Schema::Result::IngredientItem' => 'ingredient' );
-__PACKAGE__->many_to_many( items => ingredients_items => 'item' );
 
 __PACKAGE__->meta->make_immutable;
 
@@ -57,7 +55,7 @@ sub assign_to_purchase_list {
                 }
             );
 
-            $self->add_to_items($item);
+            $self->update( { item => $item->id } );
         }
     );
 }

lib/Coocook/Schema/Result/IngredientItem.pm
@@ -1,22 +1,0 @@
-package Coocook::Schema::Result::IngredientItem;
-
-use Moose;
-use MooseX::MarkAsMethods autoclean => 1;
-
-extends 'Coocook::Schema::Result';
-
-__PACKAGE__->table("ingredients_items");
-
-__PACKAGE__->add_columns(
-    ingredient => { data_type => 'int' },
-    item       => { data_type => 'int' },
-);
-
-__PACKAGE__->set_primary_key(qw<ingredient item>);
-
-__PACKAGE__->belongs_to( ingredient => 'Coocook::Schema::Result::DishIngredient' );
-__PACKAGE__->belongs_to( item       => 'Coocook::Schema::Result::Item' );
-
-__PACKAGE__->meta->make_immutable;
-
-1;

lib/Coocook/Schema/Result/Project.pm
@@ -33,7 +33,6 @@ __PACKAGE__->has_many( units          => 'Coocook::Schema::Result::Unit' );
 before delete => sub {    # TODO solve workaround
     my $self = shift;
 
-    $self->purchase_lists->items->ingredients_items->delete;
     $self->purchase_lists->items->delete;
     $self->purchase_lists->delete;
     $self->articles->article_tags->delete;

lib/Coocook/Schema/ResultSet/DishIngredient.pm
@@ -16,15 +16,7 @@ sub prepared {
 sub unassigned {
     my $self = shift;
 
-    return $self->search(
-        undef,
-        {
-            join      => 'ingredients_items',
-            distinct  => 1,
-            '+select' => { count => 'ingredients_items.item', -as => 'count_items' },
-            having => { count_items => { '=' => \'0' } },
-        }
-    );
+    return $self->search( { item => undef } );
 }
 
 1;

share/ddl/SQLite/deploy/7/001-auto-__VERSION.sql
@@ -1,0 +1,18 @@
+-- 
+-- Created by SQL::Translator::Producer::SQLite
+-- Created on Tue Sep 26 04:32:00 2017
+-- 
+
+;
+BEGIN TRANSACTION;
+--
+-- Table: dbix_class_deploymenthandler_versions
+--
+CREATE TABLE dbix_class_deploymenthandler_versions (
+  id INTEGER PRIMARY KEY NOT NULL,
+  version varchar(50) NOT NULL,
+  ddl text,
+  upgrade_sql text
+);
+CREATE UNIQUE INDEX dbix_class_deploymenthandler_versions_version ON dbix_class_deploymenthandler_versions (version);
+COMMIT;

share/ddl/SQLite/deploy/7/001-auto.sql
@@ -1,0 +1,281 @@
+-- 
+-- Created by SQL::Translator::Producer::SQLite
+-- Created on Tue Sep 26 04:32:00 2017
+-- 
+
+;
+BEGIN TRANSACTION;
+--
+-- Table: projects
+--
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY NOT NULL,
+  name text NOT NULL,
+  url_name text NOT NULL,
+  url_name_fc text NOT NULL
+);
+CREATE UNIQUE INDEX projects_name ON projects (name);
+CREATE UNIQUE INDEX projects_url_name ON projects (url_name);
+CREATE UNIQUE INDEX projects_url_name_fc ON projects (url_name_fc);
+--
+-- Table: meals
+--
+CREATE TABLE meals (
+  id INTEGER PRIMARY KEY NOT NULL,
+  project int NOT NULL,
+  date date NOT NULL,
+  name text NOT NULL,
+  comment text NOT NULL,
+  FOREIGN KEY (project) REFERENCES projects(id) ON DELETE CASCADE
+);
+CREATE INDEX meals_idx_project ON meals (project);
+CREATE UNIQUE INDEX meals_project_date_name ON meals (project, date, name);
+--
+-- Table: purchase_lists
+--
+CREATE TABLE purchase_lists (
+  id INTEGER PRIMARY KEY NOT NULL,
+  project int NOT NULL,
+  name text NOT NULL,
+  date date NOT NULL,
+  FOREIGN KEY (project) REFERENCES projects(id) ON DELETE CASCADE
+);
+CREATE INDEX purchase_lists_idx_project ON purchase_lists (project);
+CREATE UNIQUE INDEX purchase_lists_project_name ON purchase_lists (project, name);
+--
+-- Table: recipes
+--
+CREATE TABLE recipes (
+  id INTEGER PRIMARY KEY NOT NULL,
+  project int NOT NULL,
+  name text NOT NULL,
+  preparation text NOT NULL,
+  description text NOT NULL,
+  servings int NOT NULL,
+  FOREIGN KEY (project) REFERENCES projects(id) ON DELETE CASCADE
+);
+CREATE INDEX recipes_idx_project ON recipes (project);
+CREATE UNIQUE INDEX recipes_project_name ON recipes (project, name);
+--
+-- Table: shop_sections
+--
+CREATE TABLE shop_sections (
+  id INTEGER PRIMARY KEY NOT NULL,
+  project int NOT NULL,
+  name text NOT NULL,
+  FOREIGN KEY (project) REFERENCES projects(id) ON DELETE CASCADE
+);
+CREATE INDEX shop_sections_idx_project ON shop_sections (project);
+CREATE UNIQUE INDEX shop_sections_project_name ON shop_sections (project, name);
+--
+-- Table: tag_groups
+--
+CREATE TABLE tag_groups (
+  id INTEGER PRIMARY KEY NOT NULL,
+  project int NOT NULL,
+  color int,
+  name text NOT NULL,
+  comment text NOT NULL DEFAULT '',
+  FOREIGN KEY (project) REFERENCES projects(id) ON DELETE CASCADE
+);
+CREATE INDEX tag_groups_idx_project ON tag_groups (project);
+CREATE UNIQUE INDEX tag_groups_project_name ON tag_groups (project, name);
+--
+-- Table: articles
+--
+CREATE TABLE articles (
+  id INTEGER PRIMARY KEY NOT NULL,
+  project int NOT NULL,
+  shop_section int,
+  shelf_life_days int,
+  preorder_servings int,
+  preorder_workdays int,
+  name text NOT NULL,
+  comment text NOT NULL,
+  FOREIGN KEY (project) REFERENCES projects(id) ON DELETE CASCADE,
+  FOREIGN KEY (shop_section) REFERENCES shop_sections(id) ON DELETE CASCADE
+);
+CREATE INDEX articles_idx_project ON articles (project);
+CREATE INDEX articles_idx_shop_section ON articles (shop_section);
+CREATE UNIQUE INDEX articles_project_name ON articles (project, name);
+--
+-- Table: quantities
+--
+CREATE TABLE quantities (
+  id INTEGER PRIMARY KEY NOT NULL,
+  project int NOT NULL,
+  name text NOT NULL,
+  default_unit int,
+  FOREIGN KEY (default_unit) REFERENCES units(id),
+  FOREIGN KEY (project) REFERENCES projects(id) ON DELETE CASCADE
+);
+CREATE INDEX quantities_idx_default_unit ON quantities (default_unit);
+CREATE INDEX quantities_idx_project ON quantities (project);
+CREATE UNIQUE INDEX quantities_project_name ON quantities (project, name);
+--
+-- Table: tags
+--
+CREATE TABLE tags (
+  id INTEGER PRIMARY KEY NOT NULL,
+  project int NOT NULL,
+  tag_group int,
+  name text NOT NULL,
+  FOREIGN KEY (project) REFERENCES projects(id) ON DELETE CASCADE,
+  FOREIGN KEY (tag_group) REFERENCES tag_groups(id) ON DELETE CASCADE
+);
+CREATE INDEX tags_idx_project ON tags (project);
+CREATE INDEX tags_idx_tag_group ON tags (tag_group);
+CREATE UNIQUE INDEX tags_project_name ON tags (project, name);
+--
+-- Table: units
+--
+CREATE TABLE units (
+  id INTEGER PRIMARY KEY NOT NULL,
+  project int NOT NULL,
+  quantity int NOT NULL,
+  to_quantity_default real,
+  space bool NOT NULL,
+  short_name text NOT NULL,
+  long_name text NOT NULL,
+  FOREIGN KEY (project) REFERENCES projects(id) ON DELETE CASCADE,
+  FOREIGN KEY (quantity) REFERENCES quantities(id) ON DELETE CASCADE
+);
+CREATE INDEX units_idx_project ON units (project);
+CREATE INDEX units_idx_quantity ON units (quantity);
+CREATE UNIQUE INDEX units_project_long_name ON units (project, long_name);
+--
+-- Table: dishes
+--
+CREATE TABLE dishes (
+  id INTEGER PRIMARY KEY NOT NULL,
+  meal int NOT NULL,
+  from_recipe int,
+  name text NOT NULL,
+  servings int NOT NULL,
+  prepare_at_meal int,
+  preparation text NOT NULL,
+  description text NOT NULL,
+  comment text NOT NULL,
+  FOREIGN KEY (meal) REFERENCES meals(id) ON DELETE CASCADE,
+  FOREIGN KEY (prepare_at_meal) REFERENCES meals(id) ON DELETE CASCADE,
+  FOREIGN KEY (from_recipe) REFERENCES recipes(id) ON DELETE CASCADE
+);
+CREATE INDEX dishes_idx_meal ON dishes (meal);
+CREATE INDEX dishes_idx_prepare_at_meal ON dishes (prepare_at_meal);
+CREATE INDEX dishes_idx_from_recipe ON dishes (from_recipe);
+--
+-- Table: recipes_tags
+--
+CREATE TABLE recipes_tags (
+  recipe int NOT NULL,
+  tag int NOT NULL,
+  PRIMARY KEY (recipe, tag),
+  FOREIGN KEY (recipe) REFERENCES recipes(id) ON DELETE CASCADE,
+  FOREIGN KEY (tag) REFERENCES tags(id) ON DELETE CASCADE
+);
+CREATE INDEX recipes_tags_idx_recipe ON recipes_tags (recipe);
+CREATE INDEX recipes_tags_idx_tag ON recipes_tags (tag);
+--
+-- Table: articles_tags
+--
+CREATE TABLE articles_tags (
+  article int NOT NULL,
+  tag int NOT NULL,
+  PRIMARY KEY (article, tag),
+  FOREIGN KEY (article) REFERENCES articles(id) ON DELETE CASCADE,
+  FOREIGN KEY (tag) REFERENCES tags(id) ON DELETE CASCADE
+);
+CREATE INDEX articles_tags_idx_article ON articles_tags (article);
+CREATE INDEX articles_tags_idx_tag ON articles_tags (tag);
+--
+-- Table: articles_units
+--
+CREATE TABLE articles_units (
+  article int NOT NULL,
+  unit int NOT NULL,
+  PRIMARY KEY (article, unit),
+  FOREIGN KEY (article) REFERENCES articles(id) ON DELETE CASCADE,
+  FOREIGN KEY (unit) REFERENCES units(id) ON DELETE CASCADE
+);
+CREATE INDEX articles_units_idx_article ON articles_units (article);
+CREATE INDEX articles_units_idx_unit ON articles_units (unit);
+--
+-- Table: dishes_tags
+--
+CREATE TABLE dishes_tags (
+  dish int NOT NULL,
+  tag int NOT NULL,
+  PRIMARY KEY (dish, tag),
+  FOREIGN KEY (dish) REFERENCES dishes(id) ON DELETE CASCADE,
+  FOREIGN KEY (tag) REFERENCES tags(id) ON DELETE CASCADE
+);
+CREATE INDEX dishes_tags_idx_dish ON dishes_tags (dish);
+CREATE INDEX dishes_tags_idx_tag ON dishes_tags (tag);
+--
+-- Table: items
+--
+CREATE TABLE items (
+  id INTEGER PRIMARY KEY NOT NULL,
+  purchase_list int NOT NULL,
+  value real NOT NULL,
+  offset real NOT NULL DEFAULT 0,
+  unit int NOT NULL,
+  article int NOT NULL,
+  purchased bool NOT NULL DEFAULT '0',
+  comment text NOT NULL,
+  FOREIGN KEY (article) REFERENCES articles(id),
+  FOREIGN KEY (article, unit) REFERENCES articles_units(article, unit) ON DELETE CASCADE,
+  FOREIGN KEY (purchase_list) REFERENCES purchase_lists(id) ON DELETE CASCADE,
+  FOREIGN KEY (unit) REFERENCES units(id) ON DELETE CASCADE
+);
+CREATE INDEX items_idx_article ON items (article);
+CREATE INDEX items_idx_article_unit ON items (article, unit);
+CREATE INDEX items_idx_purchase_list ON items (purchase_list);
+CREATE INDEX items_idx_unit ON items (unit);
+CREATE UNIQUE INDEX items_purchase_list_article_unit ON items (purchase_list, article, unit);
+--
+-- Table: recipe_ingredients
+--
+CREATE TABLE recipe_ingredients (
+  id INTEGER PRIMARY KEY NOT NULL,
+  position int NOT NULL DEFAULT 1,
+  recipe int NOT NULL,
+  prepare bool NOT NULL,
+  article int NOT NULL,
+  unit int NOT NULL,
+  value real NOT NULL,
+  comment text NOT NULL,
+  FOREIGN KEY (article) REFERENCES articles(id) ON DELETE CASCADE,
+  FOREIGN KEY (article, unit) REFERENCES articles_units(article, unit) ON DELETE CASCADE,
+  FOREIGN KEY (recipe) REFERENCES recipes(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (unit) REFERENCES units(id) ON DELETE CASCADE
+);
+CREATE INDEX recipe_ingredients_idx_article ON recipe_ingredients (article);
+CREATE INDEX recipe_ingredients_idx_article_unit ON recipe_ingredients (article, unit);
+CREATE INDEX recipe_ingredients_idx_recipe ON recipe_ingredients (recipe);
+CREATE INDEX recipe_ingredients_idx_unit ON recipe_ingredients (unit);
+--
+-- Table: dish_ingredients
+--
+CREATE TABLE dish_ingredients (
+  id INTEGER PRIMARY KEY NOT NULL,
+  position int NOT NULL DEFAULT 1,
+  dish int NOT NULL,
+  prepare bool NOT NULL,
+  article int NOT NULL,
+  unit int NOT NULL,
+  value real NOT NULL,
+  comment text NOT NULL,
+  item int,
+  FOREIGN KEY (article) REFERENCES articles(id) ON DELETE CASCADE,
+  FOREIGN KEY (article, unit) REFERENCES articles_units(article, unit) ON DELETE CASCADE,
+  FOREIGN KEY (dish) REFERENCES dishes(id) ON DELETE CASCADE,
+  FOREIGN KEY (item) REFERENCES items(id) ON DELETE CASCADE,
+  FOREIGN KEY (unit) REFERENCES units(id) ON DELETE CASCADE
+);
+CREATE INDEX dish_ingredients_idx_article ON dish_ingredients (article);
+CREATE INDEX dish_ingredients_idx_article_unit ON dish_ingredients (article, unit);
+CREATE INDEX dish_ingredients_idx_dish ON dish_ingredients (dish);
+CREATE INDEX dish_ingredients_idx_item ON dish_ingredients (item);
+CREATE INDEX dish_ingredients_idx_unit ON dish_ingredients (unit);
+COMMIT;

commit 62edda856a9cb8e01eecf4546f3c9fbacbc7531a

Fix conversion of items if item with target unit exists
Was broken in

    75f131d Replace cross table 'ingredients_items' with FK column 'item'

If no other item exists, the item can be simply updated.

But if there already is another item with the target unit that must
be updated and the $self item needs to be deleted.

lib/Coocook/Schema/Result/Item.pm
@@ -42,18 +42,48 @@ __PACKAGE__->meta->make_immutable;
 sub convert {
     my ( $self => $unit2 ) = @_;
 
-    my $unit1 = $self->unit;
+    $self->result_source->schema->txn_do(
+        sub {
+            my $unit1 = $self->unit;
 
-    $unit1->get_column('quantity') == $unit2->get_column('quantity')
-      or die "Units not of same quantity";
+            $unit1->get_column('quantity') == $unit2->get_column('quantity')
+              or die "Units not of same quantity";
 
-    my $factor = $unit1->to_quantity_default / $unit2->to_quantity_default;
+            my $factor = $unit1->to_quantity_default / $unit2->to_quantity_default;
 
-    $self->update(
-        {
-            unit   => $unit2->id,
-            value  => $self->value * $factor,
-            offset => $self->offset * $factor,
+            my $unit2_item = $self->result_source->resultset->find(
+                {
+                    purchase_list => $self->get_column('purchase_list'),
+                    article       => $self->get_column('article'),
+                    unit          => $unit2->id,
+                }
+            );
+
+            if ($unit2_item) {
+                $unit2_item->update(
+                    {
+                        value  => $unit2_item->value + $self->value * $factor,
+                        offset => $unit2_item->offset + $self->offset * $factor,
+                    }
+                );
+
+                $self->ingredients->update( { item => $unit2_item->id } );
+
+                $self->delete;
+
+                return $unit2_item;
+            }
+            else {
+                $self->update(
+                    {
+                        unit   => $unit2->id,
+                        value  => $self->value * $factor,
+                        offset => $self->offset * $factor,
+                    }
+                );
+
+                return $self;
+            }
         }
     );
 }