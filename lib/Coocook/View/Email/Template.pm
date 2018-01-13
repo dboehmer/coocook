4af16fa294b7472c773da3b6e22068ac4faa8b5c
Daniel Böhmer <post@daniel-boehmer.de>
2018-01-13 08:49:04 +0100

    Remove static author and invalid license information

--- lib/Coocook/View/Email/Template.pm
+++ lib/Coocook/View/Email/Template.pm
@@ -13,18 +13,6 @@ Coocook::View::Email::Template - Catalyst View
 
 Catalyst View.
 
-
-=encoding utf8
-
-=head1 AUTHOR
-
-Daniel Böhmer
-
-=head1 LICENSE
-
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
-
 =cut
 
 __PACKAGE__->meta->make_immutable;
