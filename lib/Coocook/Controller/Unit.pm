dboehmer/coocook

commit a38d518bde3e1c7ceec636f4847e61f26f4a2a72

Improve code readability

lib/Coocook/Controller/Unit.pm
@@ -48,6 +48,7 @@ sub index : Chained('/project/base') PathPart('units') Args(0) {
 
     my @units;
 
+    # add delete_url to deletable units
     {
         my $action = $self->action_for('delete');
 
@@ -55,6 +56,9 @@ sub index : Chained('/project/base') PathPart('units') Args(0) {
             { join => 'quantity', order_by => [ 'quantity.name', 'long_name' ] } );
 
         while ( my $unit = $units->next ) {
+            exists $units_in_use{ $unit->id }
+              and next;
+
             $unit->quantity( $quantities{ $unit->get_column('quantity') } );
 
             my %unit = (
@@ -74,10 +78,8 @@ sub index : Chained('/project/base') PathPart('units') Args(0) {
 
             push @units, \%unit;
 
-            next if exists $units_in_use{ $unit{id} };
-
-            if ( $unit->is_quantity_default ) {
-                next if $unit->convertible_into > 0;
+            if ( $unit->is_quantity_default and $unit->convertible_into > 0 ) {
+                next;
             }
 
             $unit{delete_url} = $c->project_uri( $action, $unit{id} );

commit a89f92022425f49c6e9d723789f69533005afd2c

Fix all units in use missing on units page
Fixes #58.

Bug was carelessly introduced by me in

    a38d518 Improve code readability

lib/Coocook/Controller/Unit.pm
@@ -49,7 +49,6 @@ sub index : GET Chained('/project/base') PathPart('units') Args(0)
 
     my @units;
 
-    # add delete_url to deletable units
     {
         my $action = $self->action_for('delete');
 
@@ -57,9 +56,6 @@ sub index : GET Chained('/project/base') PathPart('units') Args(0)
             { join => 'quantity', order_by => [ 'quantity.name', 'long_name' ] } );
 
         while ( my $unit = $units->next ) {
-            exists $units_in_use{ $unit->id }
-              and next;
-
             $unit->quantity( $quantities{ $unit->get_column('quantity') } );
 
             my %unit = (
@@ -79,11 +75,11 @@ sub index : GET Chained('/project/base') PathPart('units') Args(0)
 
             push @units, \%unit;
 
-            if ( $unit->is_quantity_default and $unit->convertible_into > 0 ) {
-                next;
-            }
-
-            $unit{delete_url} = $c->project_uri( $action, $unit{id} );
+            # add delete_url to deletable units
+            exists $units_in_use{ $unit->id }    # in use, for ingredient
+              or (  $unit->is_quantity_default
+                and $unit->convertible_into > 0 )    # need to make other unit quantity default
+              or $unit{delete_url} = $c->project_uri( $action, $unit{id} );    # can be deleted
         }
     }
 

root/templates/unit/index.tt
@@ -56,7 +56,7 @@
 </td>
 [% IF unit.to_quantity_default AND NOT unit.is_quantity_default %]
     <td>[% display_value_unit(1, unit, {html=>1}) %] = [% display_value_unit(unit.to_quantity_default, default_unit, {html=>1}) %]</td>
-    <td>[% display_value_unit(unit.from_quantity_default, unit, {html=>1}) %] = [% display_value_unit(1, default_unit, {html=>1}) %]</td>
+    <td style="text-align:right">[% display_value_unit(unit.from_quantity_default, unit, {html=>1}) %] = [% display_value_unit(1, default_unit, {html=>1}) %]</td>
 [% ELSE; '<td></td><td></td>'; END %]
     <td>
 [% IF unit.quantity AND NOT unit.is_quantity_default %]