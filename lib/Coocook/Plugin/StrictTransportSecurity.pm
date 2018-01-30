bc912e26098f178cbe00da38b0abcdf7a93e6f5a
Daniel Böhmer <post@daniel-boehmer.de>
2018-01-30 14:44:04 +0100

    Refactor Plugin::StrictTransportSecurity
    - config option 'disabled' (like ActionRole::RequireSSL) instead of 'enable'
    
    - better debug output
    
    - remove override for 'enable' always on (accidentally committed development test)

--- lib/Coocook/Plugin/StrictTransportSecurity.pm
+++ lib/Coocook/Plugin/StrictTransportSecurity.pm
@@ -7,27 +7,36 @@ use Moose::Role;
 after prepare_action => sub {
     my ($c) = @_;
 
-    if ( defined $c->response->headers->header('Strict-Transport-Security') ) {
-        warn "Strict-Transport-Security header already set";
+    my $config = $c->config->{'Plugin::StrictTransportSecurity'} || {};
+
+    if ( $config->{disabled} ) {
+        $c->debug and $c->log->info("Plugin::StrictTransportSecurity is disabled by config");
         return;
     }
 
-    $c->request->uri->scheme eq 'https'
-      or return;
-
-    my $config = $c->config->{'Plugin::StrictTransportSecurity'} || {};
-
-    if ( 1 or $config->{enabled} ) {
-        my $sts = 'max-age=' . ( $config->{max_age} || 31536000 );    # default: 1 year
-
-        $config->{include_sub_domains}
-          and $sts .= '; includeSubDomains';
-
-        $config->{preload}
-          and $sts .= '; preload';
-
-        $c->response->headers->header( 'Strict-Transport-Security' => $sts );
+    if ( defined $c->response->headers->header('Strict-Transport-Security') ) {
+        $c->log->warn("Strict-Transport-Security header already set");
+        return;
     }
+
+    my $sts = 'max-age=' . ( $config->{max_age} || 31536000 );    # default: 1 year
+
+    $config->{include_sub_domains}
+      and $sts .= '; includeSubDomains';
+
+    $config->{preload}
+      and $sts .= '; preload';
+
+    if ( $c->request->uri->scheme ne 'https' ) {
+        if ( $c->debug ) {
+            $c->log->info("Would set HTTP Strict Transport Security header if HTTPS");
+            $c->log->debug("Strict-Transport-Security: $sts");
+        }
+
+        return;
+    }
+
+    $c->response->headers->header( 'Strict-Transport-Security' => $sts );
 };
 
 1;
