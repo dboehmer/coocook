2180f79b9938bb5ffdc95bf3a65b8be8f2e92af3
Daniel Böhmer <post@daniel-boehmer.de>
2018-02-02 12:27:57 +0100

    Add some error handling to Controller::Permission

--- lib/Coocook/Controller/Permission.pm
+++ lib/Coocook/Controller/Permission.pm
@@ -72,14 +72,23 @@ sub base : Chained('/project/base') PathPart('permissions') CaptureArgs(1) {
 
     $c->stash->{permission} =
       $c->project->projects_users->search( { 'user.name' => $username }, { prefetch => 'user' } )
-      ->single;
+      ->single
+      or $c->detach('/error/not_found');
 }
 
 sub edit : POST Chained('base') PathPart('edit') Args(0)
   RequiresCapability('edit_project_permission') {
     my ( $self, $c ) = @_;
 
-    $c->stash->{permission}->update( { role => $c->req->params->get('role') } );
+    my @applicable_roles = grep { $_ ne 'owner' } $c->model('Authorization')->project_roles;
+
+    my $role = $c->req->params->get('role');
+
+    if ( not grep { $_ eq $role } @applicable_roles ) {
+        $c->detach('/error/bad_request');
+    }
+
+    $c->stash->{permission}->update( { role => $role } );
 
     $c->detach('redirect');
 }
@@ -89,7 +98,6 @@ sub make_owner : POST Chained('base') PathPart('make_owner') Args(0)
     my ( $self, $c ) = @_;
 
     $c->stash->{permission}->make_owner;
-
     $c->detach('redirect');
 }
 
@@ -98,7 +106,6 @@ sub revoke : POST Chained('base') PathPart('revoke') Args(0)
     my ( $self, $c ) = @_;
 
     $c->stash->{permission}->delete;
-
     $c->detach('redirect');
 }
 
