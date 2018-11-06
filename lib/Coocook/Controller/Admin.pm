7294f744b20a97f4da19d5e53de4a395e21d5a90
Daniel Böhmer <post@daniel-boehmer.de>
2018-11-06 01:08:19 +0100

    Move FAQ controller methods to dedicated Controller::Admin::FAQ

--- lib/Coocook/Controller/Admin.pm
+++ lib/Coocook/Controller/Admin.pm
@@ -10,9 +10,9 @@ sub base : Chained('/base') PathPart('admin') CaptureArgs(0) {
 
     $c->stash(
         submenu_items => [
-            { action => 'admin/faqs',     text => "FAQ" },
-            { action => 'admin/projects', text => "Projects" },
-            { action => 'admin/users',    text => "Users" },
+            { action => 'admin/faq/index', text => "FAQ" },
+            { action => 'admin/projects',  text => "Projects" },
+            { action => 'admin/users',     text => "Users" },
         ]
     );
 }
@@ -62,83 +62,6 @@ sub users : GET HEAD Chained('base') Args(0) RequiresCapability('admin_view') {
     }
 
     $c->stash( users => \@users );
-}
-
-sub faqs : GET HEAD Chained('base') PathPart('faq') Args(0) RequiresCapability('admin_view') {
-    my ( $self, $c ) = @_;
-
-    my @faqs = $c->model('DB::FAQ')->hri->all;
-
-    for my $faq (@faqs) {
-        $faq->{url} = $c->uri_for( $self->action_for('faq'), [ $faq->{id} ] );
-    }
-
-    $c->stash(
-        faqs        => \@faqs,
-        new_faq_url => $c->uri_for( $self->action_for('new_faq') ),
-    );
-}
-
-sub new_faq : GET HEAD Chained('base') PathPart('faq/new') RequiresCapability('admin_view') {
-    my ( $self, $c ) = @_;
-
-    $c->stash(
-        admin_faq_url => $c->uri_for( $self->action_for('faqs') ),
-        submit_url    => $c->uri_for( $self->action_for('create') ),
-        template      => 'admin/faq.tt',
-    );
-}
-
-sub faq_base : Chained('base') PathPart('faq') CaptureArgs(1) {
-    my ( $self, $c, $id ) = @_;
-
-    my $faq = $c->model('DB::FAQ')->find($id)
-      or $c->detach('/error/not_found');
-
-    $c->stash( faq => $faq );
-}
-
-sub faq : GET HEAD Chained('faq_base') PathPart('') Args(0) RequiresCapability('admin_view') {
-    my ( $self, $c ) = @_;
-
-    $c->stash->{faq}{url} = $c->uri_for_action( '/faq/index', \$c->stash->{faq}->anchor );
-
-    $c->stash(
-        admin_faq_url => $c->uri_for( $self->action_for('faqs') ),
-        submit_url    => $c->uri_for( $self->action_for('update'), [ $c->stash->{faq}->id ] ),
-    );
-}
-
-sub update : POST Chained('faq_base') Args(0) RequiresCapability('admin_view') {
-    my ( $self, $c ) = @_;
-
-    $c->detach('update_or_create');
-}
-
-sub create : POST Chained('base') PathPart('faq/create') Args(0) RequiresCapability('admin_view') {
-    my ( $self, $c ) = @_;
-
-    $c->stash( faq => $c->model('DB::FAQ')->new_result( {} ) );
-
-    $c->detach('update_or_create');
-}
-
-sub update_or_create : Private {
-    my ( $self, $c ) = @_;
-
-    my $faq = $c->stash->{faq};
-
-    $faq->set_columns(
-        {
-            anchor      => $c->req->params->get('anchor'),
-            question_md => $c->req->params->get('question'),
-            answer_md   => $c->req->params->get('answer'),
-        }
-    );
-
-    $faq->update_or_insert();
-
-    $c->redirect_detach( $c->uri_for( $self->action_for('faq'), [ $faq->id ] ) );
 }
 
 __PACKAGE__->meta->make_immutable;
---
+++ lib/Coocook/Controller/Admin/FAQ.pm
@@ -0,0 +1,89 @@
+package Coocook::Controller::Admin::FAQ;
+
+use Moose;
+use MooseX::MarkAsMethods autoclean => 1;
+
+BEGIN { extends 'Coocook::Controller' }
+
+sub index : GET HEAD Chained('/admin/base') PathPart('faq') Args(0)
+  RequiresCapability('admin_view') {
+    my ( $self, $c ) = @_;
+
+    my @faqs = $c->model('DB::FAQ')->hri->all;
+
+    for my $faq (@faqs) {
+        $faq->{url} = $c->uri_for( $self->action_for('edit'), [ $faq->{id} ] );
+    }
+
+    $c->stash(
+        faqs        => \@faqs,
+        new_faq_url => $c->uri_for( $self->action_for('new_faq') ),
+    );
+}
+
+sub new_faq : GET HEAD Chained('/admin/base') PathPart('faq/new') RequiresCapability('admin_view') {
+    my ( $self, $c ) = @_;
+
+    $c->stash(
+        admin_faq_url => $c->uri_for( $self->action_for('index') ),
+        submit_url    => $c->uri_for( $self->action_for('create') ),
+        template      => 'admin/faq/edit.tt',
+    );
+}
+
+sub base : Chained('/admin/base') PathPart('faq') CaptureArgs(1) {
+    my ( $self, $c, $id ) = @_;
+
+    my $faq = $c->model('DB::FAQ')->find($id)
+      or $c->detach('/error/not_found');
+
+    $c->stash( faq => $faq );
+}
+
+sub edit : GET HEAD Chained('base') PathPart('') Args(0) RequiresCapability('admin_view') {
+    my ( $self, $c ) = @_;
+
+    $c->stash->{faq}{url} = $c->uri_for_action( '/faq/index', \$c->stash->{faq}->anchor );
+
+    $c->stash(
+        admin_faq_url => $c->uri_for( $self->action_for('index') ),
+        submit_url    => $c->uri_for( $self->action_for('update'), [ $c->stash->{faq}->id ] ),
+    );
+}
+
+sub update : POST Chained('base') Args(0) RequiresCapability('admin_view') {
+    my ( $self, $c ) = @_;
+
+    $c->detach('update_or_create');
+}
+
+sub create : POST Chained('/admin/base') PathPart('faq/create') Args(0)
+  RequiresCapability('admin_view') {
+    my ( $self, $c ) = @_;
+
+    $c->stash( faq => $c->model('DB::FAQ')->new_result( {} ) );
+
+    $c->detach('update_or_create');
+}
+
+sub update_or_create : Private {
+    my ( $self, $c ) = @_;
+
+    my $faq = $c->stash->{faq};
+
+    $faq->set_columns(
+        {
+            anchor      => $c->req->params->get('anchor'),
+            question_md => $c->req->params->get('question'),
+            answer_md   => $c->req->params->get('answer'),
+        }
+    );
+
+    $faq->update_or_insert();
+
+    $c->redirect_detach( $c->uri_for( $self->action_for('edit'), [ $faq->id ] ) );
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
