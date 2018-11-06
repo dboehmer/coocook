dboehmer/coocook

commit b6aee75277181eda3bab30810d92775d83416444

Pass along redirect URL if login fails

lib/Coocook/Controller/Session.pm
@@ -43,6 +43,8 @@ sub post_login : POST Chained('/base') PathPart('login') Args(0) {
     # is not answered in 0.x seconds ...
     sleep 1;
 
+    my $redirect = $c->req->params->get('redirect');
+
     my $user = $c->authenticate(
         {
             name           => $c->req->params->get('username'),
@@ -62,7 +64,7 @@ sub post_login : POST Chained('/base') PathPart('login') Args(0) {
             httponly => 1,             # not accessible by client-side JavaScript
         };
 
-        if ( my $redirect = $c->req->params->get('redirect') ) {
+        if ( my $redirect ) {
             $c->forward( _check_redirect_uri => [$redirect] );
 
             $c->redirect_detach( $c->uri_for_local_part($redirect) );
@@ -72,12 +74,16 @@ sub post_login : POST Chained('/base') PathPart('login') Args(0) {
     }
     else {
         $c->logout();
+
+        $c->forward( _check_redirect_uri => [$redirect] );
+
         $c->response->redirect(
             $c->uri_for_action(
                 '/login',
                 {
                     error    => "Login failed!",
                     username => $c->req->params->get('username'),
+                    $redirect ? ( redirect => $redirect ) : (),
                 }
             )
         );

commit 6bc902c6d24245d24eb04e8dfb0132935274307f

Fix redirect after login and add test for it
Broke in

    b6aee75 Pass along redirect URL if login fails

lib/Coocook/Controller/Session.pm
@@ -63,7 +63,7 @@ sub post_login : POST Chained('/base') PathPart('login') Args(0) {
             httponly => 1,             # not accessible by client-side JavaScript
         };
 
-        if ( my $redirect ) {
+        if ($redirect) {
             $c->forward( _check_redirect_uri => [$redirect] );
 
             $c->redirect_detach( $c->uri_for_local_part($redirect) );

t/user_lifecycle.t
@@ -5,7 +5,7 @@ use lib 't/lib';
 
 use DBICx::TestDatabase;
 use Test::Coocook;
-use Test::Most tests => 42;
+use Test::Most tests => 43;
 use Time::HiRes 'time';
 
 my $t = Test::Coocook->new( schema => my $schema = DBICx::TestDatabase->new('Coocook::Schema') );
@@ -201,6 +201,26 @@ subtest "password recovery marks e-mail address verified" => sub {
 };
 
 $t->login_ok( 'test', 'new, nice & shiny' );
+
+subtest "redirects after login/logout" => sub {
+    $t->get_ok('/about');
+
+    $t->logout_ok();
+
+    is $t->uri->path => '/about',
+      "client is redirected to last page after logout"
+      or diag "uri: " . $t->uri;
+
+    $t->login_fails( 'test', 'invalid' );
+
+    note "uri: " . $t->uri;
+
+    $t->login_ok( 'test', 'new, nice & shiny' );
+
+    is $t->uri->path => '/about',
+      "client is redirected to last page after login"
+      or diag "uri: " . $t->uri;
+};
 
 $t->create_project_ok( { name => "Test Project 1" } );
 