a3fb41c5864b3e22dd0b1fd2171de1fe5618c345
Mose Schmiedel <mose.schmiedel@web.de>
2019-12-25 23:52:16 +0100

    Changed  to parse the sql-File more intelligently and discover if a Statement is split over several lines. Formatted share/test_data.sql, so that it can be read easier.

--- t/lib/TestDB.pm
+++ t/lib/TestDB.pm
@@ -12,11 +12,37 @@ sub new {
 
     open my $fh, '<', 'share/test_data.sql';
 
+    my $continued_line = "";
+    my $last_line_was_continued = 0;
+
     while (<$fh>) {
         $ENV{DBIC_TRACE}
           and warn $_;
+	my $line = $_;
 
-        $schema->storage->dbh_do( sub { $_[1]->do($_) } );
+	# Remove comments from SQL-File
+	my $comment_start = index ($line, '--');
+	$line = substr ($line, 0, $comment_start);
+    # Remove trailing and leading whitespaces
+    $line =~ s/^\s+|\s+$//g;
+	next if $line =~ /^\s*$/;
+	my $last_character = substr ($line, -1);
+
+	# Check if last character of a line is not a semicolon, because in this case
+	# the line currently looked at is not a complete SQL-Statement and we can't
+	# execute it via DBICx::TestDatabase and first need to add all following lines
+	# to our statement until we find a semicolon at the end of a line.
+	if ($last_character ne ';') {
+	    $continued_line = $continued_line." ".$line; 
+	    $last_line_was_continued = 1;
+	} elsif ($last_line_was_continued) {
+	    $continued_line = $continued_line." ".$line;
+	    $schema->storage->dbh_do( sub { $_[1]->do($continued_line) } );
+	    $continued_line = "";
+	    $last_line_was_continued = 0;
+	} else {
+	    $schema->storage->dbh_do( sub { $_[1]->do($line) } );
+	}
     }
 
     close $fh;
