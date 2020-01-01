cc65b172c88ec39d03ca360c2a1fc98e72861f33
Daniel Böhmer <post@daniel-boehmer.de>
2020-01-01 15:16:32 +0100

    Fix Result::User->status() for unverified users with recovery token
    Users which have already requested a recovery token were displayed
    with status "ok".
    
    This fixes that if email_verified is NULL the user is always
    unverified.

--- lib/Coocook/Schema/Result/User.pm
+++ lib/Coocook/Schema/Result/User.pm
@@ -137,17 +137,18 @@ sub status_description { ( shift->status )[1] }
 sub status {
     my $self = shift;
 
-    if ( my $token_expires = $self->token_expires ) {
-        if ( DateTime->now <= $token_expires ) {
-            return password_recovery => sprintf "requested password recovery link (valid until %s)",
-              $token_expires;
+    if ( $self->email_verified ) {
+        if ( my $token_expires = $self->token_expires ) {
+            if ( DateTime->now <= $token_expires ) {
+                return password_recovery => sprintf "requested password recovery link (valid until %s)",
+                  $token_expires;
+            }
         }
-    }
-    elsif ( $self->token_hash ) {
-        return unverified => "e-mail address not yet verified with verification link";
+
+        return ok => "ok";
     }
 
-    return ok => "ok";
+    return unverified => "e-mail address not yet verified with verification link";
 }
 
 1;
