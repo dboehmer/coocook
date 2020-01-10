dboehmer/coocook

commit c6ab8228f0870765605e79078f91c85a53ecc7fd

Split units page into index and create, add quantities to submenu

lib/Coocook/Controller/Project.pm
@@ -47,7 +47,6 @@ sub base : Chained('/base') PathPart('project') CaptureArgs(1) {
             purchase_lists   => $c->project_uri('/purchase_list/index'),
             print            => $c->project_uri('/print/index'),
             shop_sections    => $c->project_uri('/shop_section/index'),
-            quantities       => $c->project_uri('/quantity/index'),
             units            => $c->project_uri('/unit/index'),
             import           => $c->project_uri('/project/get_import'),
         },

lib/Coocook/Controller/Quantity.pm
@@ -21,7 +21,7 @@ Catalyst Controller.
 
 =cut
 
-sub index : GET HEAD Chained('/project/base') PathPart('quantities') Args(0)
+sub index : GET HEAD Chained('/unit/submenu') PathPart('quantities') Args(0)
   RequiresCapability('view_project') {
     my ( $self, $c ) = @_;
 

lib/Coocook/Controller/Unit.pm
@@ -18,11 +18,23 @@ Catalyst Controller.
 
 =cut
 
+sub submenu : Chained('/project/base') PathPart('') CaptureArgs(0) {
+    my ( $self, $c ) = @_;
+
+    $c->stash(
+        submenu_items => [
+            { text => "All units",  action => 'unit/index' },
+            { text => "Add unit",   action => 'unit/new_unit' },
+            { text => "Quantities", action => 'quantity/index' },
+        ]
+    );
+}
+
 =head2 index
 
 =cut
 
-sub index : GET HEAD Chained('/project/base') PathPart('units') Args(0)
+sub index : GET HEAD Chained('submenu') PathPart('units') Args(0)
   RequiresCapability('view_project') {
     my ( $self, $c ) = @_;
 
@@ -95,7 +107,19 @@ sub index : GET HEAD Chained('/project/base') PathPart('units') Args(0)
     );
 }
 
-sub base : Chained('/project/base') PathPart('unit') CaptureArgs(1) {
+sub new_unit : GET HEAD Chained('submenu') PathPart('units/new') RequiresCapability('edit_project')
+{
+    my ( $self, $c ) = @_;
+
+    $c->stash(
+        template   => 'unit/new.tt',
+        create_url => $c->project_uri( $self->action_for('create') ),
+        quantities =>
+          [ $c->project->quantities->sorted->search( undef, { prefetch => 'default_unit' } )->all ],
+    );
+}
+
+sub base : Chained('submenu') PathPart('unit') CaptureArgs(1) {
     my ( $self, $c, $id ) = @_;
 
     $c->stash( unit => $c->project->units->find($id) || $c->detach('/error/not_found') );

root/templates/unit/index.tt
@@ -1,42 +1,5 @@
 [% title = "Units" %]
 
-<form method="POST" action="[% create_url %]">
-<fieldset>
-    <legend>Create new unit</legend>
-
-    <p>
-        <label for="new_short_name">Short name:</label>
-        <input  id="new_short_name" type="text" name="short_name" placeholder="short name">
-    </p>
-
-    <p>
-        <label for="new_long_name">Long name:</label>
-        <input  id="new_long_name" type="text" name="long_name" placeholder="long name">
-    </p>
-
-    <p>
-        <input type="checkbox" name="space" id="space">
-        <label for="space">prepend space</label>
-    </p>
-
-    <p>
-        <label for="new_quantity">Quantity:</label>
-        <select id="new_quantity" name="quantity">
-        [% FOR quantity IN quantities %]
-            <option value="[% quantity.id %]">[% quantity.name | html; IF quantity.default_unit; ' (default unit: '; quantity.default_unit.long_name | html; ')'; END %]</option>
-        [% END %]
-        </select>
-    </p>
-
-    <p>
-        <label for="new_to_quantity_default">Factor to quantity's default unit:</label>
-        <input  id="new_to_quantity_default" type="text" name="to_quantity_default" value="1">
-    </p>
-
-    <input type="submit" value="Create">
-</fieldset>
-</form>
-
 <table>
 <tr>
 <th>Quantity</th>

root/templates/unit/new.tt
@@ -1,0 +1,34 @@
+[% title = "Create unit" %]
+
+<form method="POST" action="[% create_url %]">
+    <p>
+        <label for="new_short_name">Short name:</label>
+        <input  id="new_short_name" type="text" name="short_name" placeholder="short name">
+    </p>
+
+    <p>
+        <label for="new_long_name">Long name:</label>
+        <input  id="new_long_name" type="text" name="long_name" placeholder="long name">
+    </p>
+
+    <p>
+        <input type="checkbox" name="space" id="space">
+        <label for="space">prepend space</label>
+    </p>
+
+    <p>
+        <label for="new_quantity">Quantity:</label>
+        <select id="new_quantity" name="quantity">
+        [% FOR quantity IN quantities %]
+            <option value="[% quantity.id %]">[% quantity.name | html; IF quantity.default_unit; ' (default unit: '; quantity.default_unit.long_name | html; ')'; END %]</option>
+        [% END %]
+        </select>
+    </p>
+
+    <p>
+        <label for="new_to_quantity_default">Factor to quantity's default unit:</label>
+        <input  id="new_to_quantity_default" type="text" name="to_quantity_default" value="1">
+    </p>
+
+    <input type="submit" value="Create">
+</form>

root/templates/wrapper.tt
@@ -59,7 +59,6 @@
     <li><a href="[% project_urls.purchase_lists %]">Purchase Lists</a></li>
     <li><a href="[% project_urls.print %]">Printing</a></li>
     <li><a href="[% project_urls.shop_sections %]">Shop sections</a></li>
-    <li><a href="[% project_urls.quantities %]">Quantities</a></li>
     <li><a href="[% project_urls.units %]">Units</a></li>
     <li><a href="[% project_urls.import %]">Import</a></li>
 [% END %]

commit 1701a2d3b34f3be02b0a14860f4ac9e2b3718604

Fix URL to quantities page on project homepage
URL was missing in this info box:

> This is a fresh project without any data. You can import data
> from other projects or start from scratch, e.g. by creating quantities.

Broken in c6ab8228f0870765605e79078f91c85a53ecc7fd.

lib/Coocook/Controller/Project.pm
@@ -52,6 +52,7 @@ sub base : Chained('/base') PathPart('project') CaptureArgs(1) {
             print            => $c->project_uri('/print/index'),
             shop_sections    => $c->project_uri('/shop_section/index'),
             units            => $c->project_uri('/unit/index'),
+            quantities       => $c->project_uri('/quantity/index'),
             import           => $c->project_uri('/project/get_import'),
             archive          => $c->project_uri('/project/archive'),
             unarchive        => $c->project_uri('/project/unarchive'),