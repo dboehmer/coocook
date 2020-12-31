cd0f35cca5e9fec859fcd178c9e2fba42706ea73
Daniel Böhmer <post@daniel-boehmer.de>
2020-12-31 09:31:40 +0100

    Fix Carp config in Coocook::Helper->uri_for() override
    Never got @CARP_NOT to work like I expected.
    
    %Carp::Internal is not as internal as it sounds.

--- lib/Coocook/Helpers.pm
+++ lib/Coocook/Helpers.pm
@@ -57,8 +57,10 @@ around uri_for => sub {
 
     return $uri if $uri;
 
-    local @Coocook::Helpers::CARP_NOT = 'Class::MOP::Method::Wrapped';
-    croak 'Catalyst->uri_for() returned undef';
+    local $Carp::Internal{'Catalyst'}                    = 1;
+    local $Carp::Internal{'Class::MOP::Method::Wrapped'} = 1;
+
+    croak "Catalyst->uri_for() returned undef for path '$_[0]'";
 };
 
 =head2 uri_for_action_if_permitted( $action, \%input?, @args? )
@@ -207,6 +209,8 @@ sub project_uri {
 
     # if last argument is hashref that's the \%query_values argument
     my @query = ref $_[-1] eq 'HASH' ? pop @_ : ();
+
+    local $Carp::Internal{ __PACKAGE__() } = 1;
 
     return $c->uri_for_action( $action, [ $project->id, $project->url_name, @_ ], @query );
 }
