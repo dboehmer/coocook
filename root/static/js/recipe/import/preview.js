6fdc259f1acf1dade89c0cc8ea911c8f45efe9fd
Timon Foerster <timon@webfoerster.de>
2022-05-30 23:02:37 +0200

    (#155) code of preview.js replaced with vanilla js

--- root/static/js/recipe/import/preview.js
+++ root/static/js/recipe/import/preview.js
@@ -1,47 +1,56 @@
-$( function() {
-    $('form#import table td.article select').change(function() {
-        let $this = $(this);
+(() => {
+    const articleElemList = document.querySelectorAll('form#import table td.article select');
+    articleElemList.forEach(articleElem => {
+        articleElem.onchange = () => {
+            let selectedArticleElem = articleElem.options[articleElem.options.selectedIndex];
+        
+            let unitElem = articleElem.closest('tr').querySelector('td.unit select');
+            let unitOptionElems = unitElem.options;
 
-        let $selectedOption = $this.find('option:selected');
-
-        let $unit = $this.closest('tr').find('td.unit select');
-        let $units = $unit.find('option');
-
-        if( !$selectedOption.val() ) {
-            $units.show();
-            return;
+            if( !selectedArticleElem.value ) {
+                Array.from(unitOptionElems).forEach(unitOptionElem => {
+                    unitOptionElem.style.display = 'block';
+                });
+                return;
+            }
+    
+            let unitIds = selectedArticleElem.getAttribute('data-units').split(',');
+            // if select unit is not applicable to article, reset unit selector
+            if( unitIds.indexOf( unitElem.value ) == -1 ) {
+                unitElem.value = '';
+            }
+    
+            // show only applicable units
+            Array.from(unitOptionElems).forEach(unitOptionElem => {
+                let unitId = unitOptionElem.value;
+                if( unitId == '' || unitIds.indexOf(unitId) > -1 ) {
+                    unitOptionElem.style.display = 'block';
+                } else {
+                    unitOptionElem.style.display = 'none';
+                }
+            });
         }
-
-        let unitIds = $selectedOption.attr('data-units').split(',');
-
-        // if select unit is not applicable to article, reset unit selector
-        if( unitIds.indexOf( $unit.val() ) == -1 ) {
-            $unit.val('');
-        }
-
-        // show only applicable units
-        $units.each( function() {
-            let $u = $(this);
-            let unitId = $u.val();
-
-            $u.toggle( unitId == '' || unitIds.indexOf(unitId) > -1 );
-        });
-    }).trigger('change');
+    });
 
     // check uniqueness of recipe name
-    $('form#import input[name="name"]').on('change input', function() {
-        let name = this.value;
-
-        this.setCustomValidity( existingRecipeNames.indexOf(name) == -1 ? '' : "This recipe name already exists in this project" );
-    }).trigger('input');
+    const nameInputElem = document.querySelector('form#import input[name="name"]');
+    nameInputElem.addEventListener('input', () => {
+        let name = nameInputElem.value;
+        
+        nameInputElem.setCustomValidity( existingRecipeNames.indexOf(name) == -1 ? '' : "This recipe name already exists in this project" );
+    });
 
     // skip checkboxes
-    $('form#import table td.import input[type="checkbox"]').on( 'change', function() {
-        let skip = !this.checked;
-        let $tr = $(this).closest('tr');
+    const checkboxElemList = document.querySelectorAll('form#import table td.import input[type="checkbox"]');
+    checkboxElemList.forEach(checkboxElem => {
+        checkboxElem.onchange = () => {
+            let skip = !checkboxElem.checked;
+            let trElem = checkboxElem.closest('tr');
 
-        $tr.toggleClass( 'skip', skip );
+            trElem.classList.toggle('skip');
 
-        $tr.find('input,select').not(this).prop( 'disabled', skip );
-    }).trigger('change');
-} );
+            Array.from(trElem.querySelectorAll('input,select')).filter((item) => item != checkboxElem)
+                .forEach(item => skip ? item.setAttribute( 'disabled', skip ) : item.removeAttribute('disabled') );
+        }
+    });
+})();
