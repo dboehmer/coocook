0201b3acb1940607fd5bb7a511d86e3095ebf64b
Timon Foerster <timon@webfoerster.de>
2022-06-11 16:18:43 +0200

    bootstrapification of login, register and recover

--- root/static/js/user/register.js
+++ root/static/js/user/register.js
@@ -5,15 +5,17 @@
 
     const passwordElem  = document.querySelector('input[name="password"]');
     const password2Elem = document.querySelector('input[name="password2"]');
-    const meterElem =  document.createElement('span');
-    passwordElem.parentElement.appendChild(meterElem);
+    const meterElem =  document.getElementById('meter');
+
+    let html = 'strength: ' + '&#x2606;'.repeat( MAX_STARS );
+    meterElem.innerHTML = html;
 
     passwordElem.addEventListener('input', () => {
         let password = passwordElem.value;
 
         let stars = password == '' ? 0 : ( zxcvbn(password).score + 1 );
 
-        let html = 'strength: ';
+        html = 'strength: ';
         html += '&#x2605;'.repeat(             stars );
         html += '&#x2606;'.repeat( MAX_STARS - stars );
 
@@ -21,15 +23,14 @@
         meterElem.setAttribute( 'title', stars + ' of ' + MAX_STARS );
     });
 
-    const comparatorElem = document.createElement('span');
-    password2Elem.parentElement.appendChild(comparatorElem);
+    const comparatorElem = document.getElementById('comparator');
 
     [passwordElem, password2Elem].forEach(item => {
         item.addEventListener('input', () => {
             let password  = passwordElem.value;
             let password2 = password2Elem.value;
 
-            if( password.length || password2.length ) {
+            if( (password.length && password2.length) || (password2.length) ) {
                 comparatorElem.innerHTML = password == password2 ? "matches" : "doesn't match";
             }
             else {
